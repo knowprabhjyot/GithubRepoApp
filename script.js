21e8de88940cc295ae63de34493905a98d37a543
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2024-07-02 17:54:54 -0700

    closes #6 Show list of followers

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ async function getUsersByKeyword(event) {
 
 function generateUserCards(userArray) {
   const userContainer = document.getElementById("user-container");
-  userContainer.innerHTML = '';
-  
+  userContainer.innerHTML = "";
+
   for (let user of userArray) {
     // Generate a UI card
 
@@ -23,18 +23,41 @@ function generateUserCards(userArray) {
     <div class="flex justify-end px-4 pt-4">
     </div>
     <div class="flex flex-col items-center pb-10">
-        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="${user.avatar_url}" alt="Bonnie image"/>
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">${user.login}</h5>
+        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="${
+          user.avatar_url
+        }" alt="Bonnie image"/>
+        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">${
+          user.login
+        }</h5>
         <div class="flex mt-4 md:mt-6 gap-4">
-            <a href="${user.followers_url}" target="_blank" class="items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Followers</a>
+            <a onclick=getFollowers(${JSON.stringify(
+              user.login
+            )}) target="_blank" class="items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Followers</a>
             <a href="#" target="_blank" class="items-center px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-blue-800">Repos</a>
         </div>
+
+     <div id="user_${user.login}" class="grid grid-cols-4 gap-4">
+     </div>
     </div>
 </div>
-
         `;
     // show the data for that particular user on that UI card
 
     userContainer.innerHTML += UICard;
   }
+}
+
+async function getFollowers(user) {
+  const FOLLOWERS_URL = `https://api.github.com/users/${user}/followers`;
+
+  const followers = await fetch(FOLLOWERS_URL);
+  const followersInJson = await followers.json();
+
+  const userCard = document.getElementById(`user_${user}`);
+
+  for (let follower of followersInJson) {
+    const img = `<img class="rounded-full" width="30px" src="${follower.avatar_url}">`;
+    userCard.innerHTML += img;
+  }
+
 }
