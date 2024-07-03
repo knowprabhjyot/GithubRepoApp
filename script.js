723e53dd150b13b07686372971f91fe0905ddad6
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2024-07-02 17:19:56 -0700

    closes #5 Fixed few bugs

--- script.js
+++ script.js
@@ -13,21 +13,21 @@ async function getUsersByKeyword(event) {
 
 function generateUserCards(userArray) {
   const userContainer = document.getElementById("user-container");
-
+  userContainer.innerHTML = '';
+  
   for (let user of userArray) {
     // Generate a UI card
 
     const UICard = `
-
-<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <div class="flex justify-end px-4 pt-4">
     </div>
     <div class="flex flex-col items-center pb-10">
         <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="${user.avatar_url}" alt="Bonnie image"/>
         <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">${user.login}</h5>
-        <div class="flex mt-4 md:mt-6">
-            <a href="${user.followers_url}" target="_blank" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Followers</a>
-            <a href="${user.following_url}" target="_blank" class="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Following</a>
+        <div class="flex mt-4 md:mt-6 gap-4">
+            <a href="${user.followers_url}" target="_blank" class="items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Followers</a>
+            <a href="#" target="_blank" class="items-center px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-blue-800">Repos</a>
         </div>
     </div>
 </div>
